95106fbf265f0aba02e01be3fbbd05f676fe339e
Blooming-Cactus <leonardoriband@gmail.com>
2022-01-02 16:23:35 -0300

    Adiciona imagens e uma navbar funcional, e faz correções

--- index.js
+++ index.js
@@ -8,6 +8,12 @@ observer = new IntersectionObserver(function(entries, observer){
     entries.forEach(entry=> entry.isIntersecting ? entry.target.style.animationPlayState="running" : "");
 }, observer_options)
 
+for (let i=0; i< document.getElementsByTagName("h2").length; i++){
+    observer.observe(document.getElementsByTagName("h2")[i]);
+}
+
 for (let i=0; i< document.getElementsByTagName("span").length; i++){
     observer.observe(document.getElementsByTagName("span")[i]);
 }
+
+observer.observe(document.getElementById("left-point"));
